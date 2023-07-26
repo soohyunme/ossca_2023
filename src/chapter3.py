3604d433395866d50a09c175b40159e42cddaca2
soohyunme <soohyun.dev@gmail.com>
2023-07-27 00:29:58 +0900

    feat: chapter7 todo 3-d

--- src/chapter3.py
+++ src/chapter3.py
@@ -1,7 +1,9 @@
+import joblib
 from flask import Flask, jsonify, request
 from PIL import Image
 
 app = Flask(__name__)
+study_pred_model = joblib.load("./regression_model.pkl")
 
 
 @app.route("/")
@@ -15,19 +17,31 @@ def hello():
     return "Hello World"
 
 
-# In postman -> [POST] http://localhost:5000/echo, {body - raw - json} -> {"parameter": "test"}  - 4
+# In postman -> [POST] http://localhost:5000/echo
+# {body - raw - json} -> {"parameter": "test"}  - 4
 @app.route("/echo", methods=["POST"])
 def get_echo():
     param = request.get_json()
     return jsonify(param)
 
 
-# In postman -> [POST] http://localhost:5000/upload_image, {form-data} -> key=file, value=test.jpg - 5
+# In postman -> [POST] http://localhost:5000/upload_image
+# {form-data} -> key=file, value=test.jpg - 5
 @app.route("/upload_image", methods=["POST"])
 def upload_image():
     img = Image.open(request.files["file"])
     width, height = img.size
     return jsonify({"width": width, "height": height})
+
+
+# chapter 7 todo 4
+@app.route("/upload_image", methods=["POST"])
+def inference_study():
+    # # Predict sample data - 7
+    # X_sample = np.array([12, 14]).reshape(-1, 1)
+    # y_sample_pred = study_pred_model.predict(X_sample)
+
+    return 0
 
 
 if __name__ == "__main__":
